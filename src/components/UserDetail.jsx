8ce46aae27b132fee6c63573aa26b413b0835f72
EricBenitez01 <leonetkd@gmail.com>
2023-01-07 11:43:49 -0300

    better repos screen

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -48,7 +48,7 @@ async function repositoris(){
   }
   console.log(repos)
   return( 
-    <div className='conteiner mx-auto bg-zinc-900 p-10  grid h-screen place-items-center'key={detail.id}>
+  <div className='conteiner mx-auto bg-zinc-900 p-10 h-full grid h-screen place-items-center'key={detail.id}>
     <div className='cardDetail w-70'>
       <h2>profile detail</h2>
       <p>{detail.id}</p>
@@ -56,21 +56,25 @@ async function repositoris(){
       <p>{detail.url}</p>
       <p>{detail.created_at}</p>
     </div>
-    <div className='repoDetail'>
-      <h2>github repositoris</h2>
-      {repos.map((repo) => 
-        <div >    
-            <p>{repo.id}</p>
-            <p>{repo.login}</p>
-            <p>{repo.url}</p>
-            <p>{repo.name}</p>     
-        </div>      
-      )}
-    </div>  
-    </div>
+    <div>   
+    <h2 className='text-xl font-bolt capitalize' >REPOSITORY </h2>       
+        <div className='grid grid-cols-5 gap-2 p-4'> 
+              {repos.map((repo) => 
+                 /* ID, name, and URL to the repository. */
+                <div key={repo.id} className='bg-violet-900 text-white rounded-lg p-5 text-justify break-words'>
+                <h1 className='text-xl font-bolt capitalize ' >ID: {repo.id}</h1> 
+                <p className='text-gray-500 text-sm'>Name: {repo.name} </p>  
+                <p className='text-gray-500 text-sm'>URL:{repo.url} </p>
+                  
+                </div>  
+                )}
+        </div>   
+      </div>  
+      
+  </div>  
+  
     
   );
-  
 };
 
 export default UserDetail;
