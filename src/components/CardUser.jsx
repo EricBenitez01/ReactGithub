1dec700422c96f7ccd6679e658303569584816e4
EricBenitez01 <leonetkd@gmail.com>
2023-01-07 20:58:07 -0300

    d

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 function CardUser({user}) {
   return (
     <div className='bg-gray-800 text-white rounded-lg p-5'>
-    <h1 className='text-xl font-bolt capitalize ' >{user.id}</h1> 
-    <p className='text-gray-500 text-sm'> {user.login} </p>
+    <h1 className='text-xl font-bolt capitalize ' >User ID{user.id}</h1> 
+    <p className='text-gray-500 text-sm'> User Name{user.login} </p>
     </div>
   )
 }
